28526184c09649976b4faba694bfd2a786d9dcfc
Vachagan <vache.grg40@gmail.com>
2020-02-03 12:46:12 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,29 +1,30 @@
 import json
 
-def sort_list(new_list):
-    return [(key, val if not isinstance(val, dict) else sort_list(val)) for key, val in new_list.items()]
+
+def _indent(s):
+    return ''.join(s.splitlines())
+
+
+def _format_value(value):
+    return _indent(_generate_pprint_json(value))
+
+
+def _generate_pprint_json(value):
+    return json.dumps(value, sort_keys=True, indent=0)
 
 
 def _bottom_up_sort(unsorted_json):
-
     if isinstance(unsorted_json, list):
-        new_list = [_bottom_up_sort(element) for element in unsorted_json]
-        if any(isinstance(el, dict) for el in new_list):
-            return sorted(new_list, key=sort_list)
-        return sorted(new_list)
+        return sorted([_bottom_up_sort(item) for item in unsorted_json])
 
     elif isinstance(unsorted_json, dict):
-        new_dict = {key:_bottom_up_sort(unsorted_json[key]) for key in sorted(unsorted_json)}
-        return new_dict
+        return {key:_bottom_up_sort(unsorted_json[key]) for key in sorted(unsorted_json)}
 
-    else:
-        return unsorted_json
-
-
+    return unsorted_json
 
 
 def main():
     str_json = open("json_file.json").read()
-    return _bottom_up_sort(json.loads(str_json))
+    return _format_value(_bottom_up_sort(json.loads(str_json)))
 
-print(main())
+print main()
